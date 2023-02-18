58e0eed0a508fe742cd06a46503e5e4f0f72fc3c
Nicknan <jurawankp@gmail.com>
2023-02-18 15:55:18 +0700

    1

--- connect.php
+++ connect.php
@@ -5,11 +5,20 @@ $password = "123456";
 $db_name = "bankdb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db_name);
+//$conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "เชื่อมต่อระบบฐานข้อมูลสำเร็จ";
+//if ($conn->connect_error) {
+ // die("Connection failed: " . $conn->connect_error);
+//}
+//echo "เชื่อมต่อระบบฐานข้อมูลสำเร็จ";
+
+$link = mysqli_connect($servername, $username, $password, $db_name);
+if(!$link)
+echo "Connection failed";
+
+date_default_timezone_set("Asia/Bangkok");
+if(!$link->set_charset("utf8"))
+echo "Can't set charector UTF"
+
 ?>
