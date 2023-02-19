563b8ce1731128d6b7eddc9ceae0a042c1be8692
Nicknan <jurawankp@gmail.com>
2023-02-19 14:34:58 +0700

    1

--- login.php
+++ login.php
@@ -27,13 +27,14 @@
         <img src="./images/login1.png" width="100px" height="100px"><br>
         <br>
         <br>
-        Username : <input type = "text" name = "account_name"><br>
+        Username : <input type = "text" name = "account_name" placeholder ="your username"> <br>
         Password : <input type = "text" name = "password"><br>
         <br>
         <input type = "submit" value = "Login">
         <br>  
         <a href="forget.php">FORGET</a>
-        <a href="create.php?">CREATE</a>              
+        <a href="create.php?">CREATE</a>  
+                    
     </form> 
 </body>
 </html>
