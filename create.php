d039c040d47682bf03bc63c7c947e53a3fb56664
Nicknan <jurawankp@gmail.com>
2023-02-19 15:46:17 +0700

    1

--- create.php
+++ create.php
@@ -14,25 +14,30 @@
         width: 100%;
         height: 100vh;
     }
+    label{
+        display: inline-block;
+        width: 100px;
+        margin-bottom: 10px;
+    }
 </style>
 <body>
 
 <form action="save_create_people.php" method="post">
-    ID Card : <input type="text" name="id_card" placeholder ="เลขบัตรประจำตัวประชาชน"><br>
-    Titie name : <select name="titlename">
+    <label>ID Card :</label> <input type="text" name="id_card" placeholder ="เลขบัตรประจำตัวประชาชน"><br>
+    <label>Titie name :</label> <select name="titlename">
         <option value="select">select</option>
         <option value="Mr">Mr</option>
         <option value="Mrs">Mrs</option>
         <option value="Ms">Ms</option>
     </select><br>
-    First name - Last name : <input type="text" name="name"placeholder ="ชื่อจริง-สกุลจริง"><br>
-    Phon number : <input type="text" name="number" placeholder ="+66"><br>
-    Email : <input type="text" name="email" placeholder ="example@mail.com"><br>
-    Birthday : <input type="date" name="birthday"><br>
-    Age : <input type="text" name="age"placeholder ="อายุ"><br>
-    Account name : <input type="text" name="account_name"placeholder ="ชื่อบัญชี"> <br>
-    Money : <input type="text" name="money" placeholder ="เงินฝากเริ่มต้น"><br>
-    Address : <input type="text" name="address" placeholder ="Bangkok Thailand"><br>
-    Password : <input type="text" name="password" placeholder ="รหัสผ่าน"><br>
+    <label>Fullname :</label> <input type="text" name="name"placeholder ="ชื่อจริง-สกุลจริง"><br>
+    <label>Phon number :</label> <input type="text" name="number" placeholder ="+66"><br>
+    <label>Email :</label> <input type="text" name="email" placeholder ="example@mail.com"><br>
+    <label>Birthday :</label> <input type="date" name="birthday"><br>
+    <label>Age :</label> <input type="text" name="age"placeholder ="อายุ"><br>
+    <label>Account name :</label> <input type="text" name="account_name"placeholder ="ชื่อบัญชี"> <br>
+    <label>Money :</label> <input type="text" name="money" placeholder ="เงินฝากเริ่มต้น"><br>
+    <label>Address :</label> <input type="text" name="address" placeholder ="Bangkok Thailand"><br>
+    <label>Password :</label> <input type="text" name="password" placeholder ="รหัสผ่าน"><br>
     <input type="submit" value="CREATE">
 </form>
