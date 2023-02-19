528ea04e64afa7536d1ed2d468e38c18bb5d0dc0
Nicknan <jurawankp@gmail.com>
2023-02-19 20:27:18 +0700

    1

--- edit_people.php
+++ edit_people.php
@@ -1,5 +1,29 @@
 <!DOCTYPE html>
 <html>
+<style>
+    body {
+       
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background:#708090;
+        height: calc(var(--vh, 1vh) * 100);
+        overflow: hidden;
+        width: 100%;      
+    }
+    label{
+        display: inline-block;
+        width: 150px;
+        margin-bottom: 10px;
+        font-family:verdana;
+        color: #000033;
+    }
+    p{
+    	display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
 <body>
 <?php
     require("connect.php");
@@ -12,20 +36,20 @@
 ?>  
 <form action="save_edit_people.php" method="post">
     <input type="hidden" name="account_id" value="<?php print($row["account_id"]); ?>">
-    Titie name : <select name="titlename">
+    <label>Titie name :</label> <select name="titlename">
         <option value="select"<?php if($row["titlename"]=="select") print("selected"); ?>>select</option>
         <option value="Mr"<?php if($row["titlename"]=="Mr") print("selected"); ?>>Mr</option>
         <option value="Mrs"<?php if($row["titlename"]=="Mrs") print("selected"); ?>>Mrs</option>
         <option value="Ms"<?php if($row["titlename"]=="Ms") print("selected"); ?>>Ms</option>
     </select><br>
-    First name - Last name : <input type="text" name="name" value="<?php print($row["name"]); ?>"><br>
-    Phon number : <input type="text" name="number" value="<?php print($row["number"]); ?>"><br>
-    Email : <input type="text" name="email" value="<?php print($row["email"]); ?>"><br>
-    Age : <input type="text" name="age" value="<?php print($row["age"]); ?>"><br>
-    Account name : <input type="text" name="account_name" value="<?php print($row["account_name"]); ?>"><br>
-    Address : <input type="text" name="address" value="<?php print($row["address"]); ?>"><br>
-    Password : <input type="text" name="password" value="<?php print($row["password"]); ?>"><br>
-    <input type="submit" value="EDIT">
+    <label>First name - Last name :</label> <input type="text" name="name" value="<?php print($row["name"]); ?>"><br>
+    <label>Phon number :</label> <input type="text" name="number" value="<?php print($row["number"]); ?>"><br>
+    <label>Email :</label> <input type="text" name="email" value="<?php print($row["email"]); ?>"><br>
+    <label>Age :</label> <input type="text" name="age" value="<?php print($row["age"]); ?>"><br>
+    <label>Account name :</label> <input type="text" name="account_name" value="<?php print($row["account_name"]); ?>"><br>
+    <label>Address :</label> <input type="text" name="address" value="<?php print($row["address"]); ?>"><br>
+    <label>Password :</label> <input type="text" name="password" value="<?php print($row["password"]); ?>"><br>
+    <p><input type="submit" value="EDIT" style="background-color:#003366; color:white; font-size:15px; border-radius:5px; font-family:verdana;"></p>
 </form>
 </body>
 </html>
