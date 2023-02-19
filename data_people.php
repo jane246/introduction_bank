4deac3cf8d215ef625f110735f565d2ca10b94c1
Jane246 <janejennie042@gmail.com>
2023-02-19 14:50:33 +0700

    16

--- data_people.php
+++ data_people.php
@@ -2,10 +2,6 @@
 <html>
 
 <body>    
-    <a href="edit_people.php?account_id=<?php echo $account_id; ?>">EDIT</a><br>
-    <a href="delete_people.php?account_id=<?php echo $account_id; ?>">DELETE</a><br>
-    <a>BANK</a><br>
-    <a href="index.php">LOGOUT</a><br>
     <?php
     require("connect.php");
     $account_id=$_GET["account_id"];
@@ -22,6 +18,11 @@
         }
     }
     ?>
+    <a href="edit_people.php?account_id=<?php echo $account_id; ?>">EDIT</a><br>
+    <a href="delete_people.php?account_id=<?php echo $account_id; ?>">DELETE</a><br>
+    <a>BANK</a><br>
+    <a href="index.html">LOGOUT</a><br>
+
     <a href="deposit.php?account_id=<?php echo $account_id; ?>">DEPOSIT</a><br>
     <a href="withdrew.php?account_id=<?php echo $account_id; ?>">WITHDRAW</a><br>
     <a href="tranfer.php?account_id=<?php echo $account_id; ?>">TRANSFER</a><br>
--- deposit.php
+++ deposit.php
@@ -2,18 +2,16 @@
 <html>
 
 <body>  
-
-    Money <input type = "text" name = "deposit"><br> 
+    Money <input type = "text" name = "deposit"><br>
     <?php
     require("connect.php");
     $account_id=$_GET["account_id"];
-
-    $sql = "UPDATE bank set $money = $money+200 WHERE account_id='$account_id'";
+    $deposit=$_GET["deposit"] ;
+    $deposit = $row['money'] + deposit;
+    $sql = "UPDATE bank set money = '{$deposit}' WHERE account_id='$account_id'";
     $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-        }
-    }
+    if ($result->num_rows > 0)
+        $row = $result->fetch_assoc();
     ?>
 </body>
 </html>
