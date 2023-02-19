26ebfc567e924560e4c3a53ef0774cc0632a5764
Nicknan <jurawankp@gmail.com>
2023-02-19 14:07:38 +0700

    1

--- login.php
+++ login.php
@@ -1,7 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>BANK</title>
+<title>ลงชื่อเข้าใช้</title>
+<style>
+    body {
+        --primary: 25,91,255;
+        --color: 44, 62, 80;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        background: #f5b8e7;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: calc(var(--vh, 1vh) * 100);
+        overflow: hidden;
+        color: rgb(var(--color));
+        width: 100%;
+        height: 100vh;
+    }
+</style>
 </head>
 <body>
     <form action="checklogin.php" method="post">
