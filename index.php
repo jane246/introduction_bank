ff638ed811b0ab2f4d31d235c7f980ad3a594916
Nicknan <jurawankp@gmail.com>
2023-02-19 20:47:52 +0700

    1

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
         font-family:Impact;
         box-sizing: border-box;
         color:white;
-        font-size: 200%
+        font-size: 250%
     } 
     button {
         padding: 20px 50px;
