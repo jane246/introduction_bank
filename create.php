436b45b9c9402d9e31b97f4c66ad833d76df8465
Jane246 <janejennie042@gmail.com>
2023-02-25 09:07:16 +0700

    23

--- create.php
+++ create.php
@@ -31,22 +31,26 @@
 <body>
 <img src="./images/12.png" align-items="left" width="35%" height="85%">
 <form action="save_create_people.php" method="post">
-    <label>ID Card :</label> <input type="text" name="id_card" placeholder ="เลขบัตรประจำตัวประชาชน"><br>
-    <label>Titie name :</label> <select name="titlename">
-        <option value="select">select</option>
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-    </select><br>
-    <label>Fullname :</label> <input type="text" name="name"placeholder ="ชื่อจริง-สกุลจริง"><br>
-    <label>Phon number :</label> <input type="text" name="number" placeholder ="+66"><br>
-    <label>Email :</label> <input type="text" name="email" placeholder ="example@mail.com"><br>
-    <label>Birthday :</label> <input type="date" name="birthday"><br>
-    <label>Age :</label> <input type="text" name="age"placeholder ="อายุ"><br>
-    <label>Account name :</label> <input type="text" name="account_name"placeholder ="ชื่อบัญชี"> <br>
-    <label>Money :</label> <input type="text" name="money" placeholder ="เงินฝากเริ่มต้น"><br>
-    <label>Address :</label> <input type="text" name="address" placeholder ="Bangkok Thailand"><br>
-    <label>Password :</label> <input type="text" name="password" placeholder ="รหัสผ่าน"><br>
+    <label>ID Card :</label> <input type="text" name="id_card" placeholder ="เลขบัตรประจำตัวประชาชน" required><br>
+    <!--<label>Titie name :</label> <select name="titlename" required>
+        <option value="" required>select</option>
+        <option value="Mr" required>Mr</option>
+        <option value="Mrs" required>Mrs</option>
+        <option value="Miss" required>Miss</option>
+    </select><br>-->
+    <label>Titie name :</label>
+    <input type="radio" name="titlename" value="Mr" required>Mr</input>
+    <input type="radio" name="titlename" value="Mrs" required>Mrs</input>
+    <input type="radio" name="titlename" value="Miss" required>Miss</input><br>
+    <label>Fullname :</label> <input type="text" name="name"placeholder ="ชื่อจริง-สกุลจริง" required><br>
+    <label>Phon number :</label> <input type="text" name="number" placeholder ="+66" required><br>
+    <label>Email :</label> <input type="text" name="email" placeholder ="example@mail.com" required><br>
+    <label>Birthday :</label> <input type="date" name="birthday" required><br>
+    <label>Age :</label> <input type="text" name="age"placeholder ="อายุ" required><br>
+    <label>Account name :</label> <input type="text" name="account_name"placeholder ="ชื่อบัญชี" required> <br>
+    <label>Money :</label> <input type="text" name="money" placeholder ="เงินฝากเริ่มต้น" required><br>
+    <label>Address :</label> <input type="text" name="address" placeholder ="Bangkok Thailand" required><br>
+    <label>Password :</label> <input type="text" name="password" placeholder ="รหัสผ่าน" required><br>
     <p><input type="submit" value="CREATE" style="background-color:#003366; color:white; font-size:15px; border-radius:5px; font-family:verdana;"></p>
 </form>
 </html>
