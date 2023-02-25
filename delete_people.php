ba8552d1753007ef95accccb1eacbef7d3f550da
Jane246 <janejennie042@gmail.com>
2023-02-25 19:53:29 +0700

    5

--- delete_people.php
+++ delete_people.php
@@ -5,6 +5,9 @@ $account_id=$_GET["account_id"];
 
 $sql="DELETE FROM bank WHERE account_id='$account_id'";
 $conn->query($sql);
+
+$sql="DELETE FROM statement";
+$conn->query($sql);
 $conn->close();
 header( "location: index.php" );
 ?>
