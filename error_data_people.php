abdb782bf8c5dd965cc09ac3d42cf00a0b61f917
Nicknan <jurawankp@gmail.com>
2023-02-26 10:27:27 +0700

    1

--- error_data_people.php
+++ error_data_people.php
@@ -1,5 +1,27 @@
 <!DOCTYPE html>
 <html>
+<style>
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        background: #a4b6d7;
+        overflow: hidden;
+    }
+    a1{
+        font-family:Impact;
+        font-size: 300%;
+    }
+    p{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    a{
+        font-size: 150%;
+    }
+</style>
 <body> 
 
     <form>
