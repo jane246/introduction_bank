a01ffdf19ca1586a27c81e6b174de27231559e5a
Jane246 <janejennie042@gmail.com>
2023-02-26 13:48:19 +0700

    1

--- tranfer.php
+++ tranfer.php
@@ -60,10 +60,15 @@
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
+        echo "Confirm money transfer to ".$row['account_name'];
+    }
+    else
+    {
+        echo "Can't find";
     }
     ?>
     <form action="tranfer.php?account_id=<?php if (isset($account_id)) {echo $account_id;} ?>&person=<?php if (isset($to_account_id)) {echo $to_account_id;} ?>&M=<?php if (isset($M)) {echo $M;} ?>&comment=<?php if (isset($comment)) {echo $comment;} ?>&" method="post">     
-            <p>tranfer <?php if (isset($to_account_id)) {echo $to_account_id;} ?>"</p>
+            <!--<p>tranfer <?php if (isset($to_account_id)) {echo $to_account_id;} ?>"</p>-->
             <h1><input type="submit" value="OK" style="background-color:#003366; color:white; font-size:13px; border-radius:5px; font-family:verdana;"></h1>
             <hr>
         </form>
